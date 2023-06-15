0b25f1df5b63f157a4381c0472ac6252a361cb4c
Cristina Rodriguez <yosola@gmail.com>
2023-06-15 14:38:27 -0700

    Form Validation working

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import WeatherForm from './components/form'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <WeatherForm/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/form.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+
+const WeatherForm = (props) =>{
+
+  const [city, setCity] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState("");
+
+  const validateInput = () => {
+    const cityRegex = /^[a-zA-Z\s]+,\s*[a-zA-Z\s]+$/;
+    const isValidCity = cityRegex.test(city.trim());
+    setIsValid(isValidCity);
+  }
+
+
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    validateInput();
+    if(isValid){
+      console.log(city);
+      setCity(" ");
+    } else{
+      setError("Please enter a city with the format City, State");
+    }
+  }
+
+
+    return (
+        <div className="weather">
+        <h1 className="App-header">Techtonica Weather Forecast App</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="city-name"
+            type="text"
+            placeholder="Please enter City, State  "
+            name="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
+        <div>
+        {!error ? (null) : (<p>{error}</p>)}
+        </div>
+      </div>
+    )
+}
+
+export default WeatherForm;
